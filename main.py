1e43053092d722b81b3fc45f41549c62b5de224e
register-web <noreply@github.com>
2025-11-23 21:35:52 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import telebot
+from telebot import types
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(
+        types.InlineKeyboardButton(
+            text="инфа и как попасть на сервер",
+            url="https://realm-webapp.vercel.app/"
+        )
+    )
+
+    text = (
+        "Welcome to Realm!🎉🎉🎉\n\n"
+        "Realm — майнкрафт проект/сервер, нацеленный на убийство вашего свободного времени☺️\n\n"
+        "Если кратко:\n"
+        "интересно повыживать как в обычных дружеских играх на недельку,\n"
+        "только с минимальными ограничениями для поддержки сервера,\n"
+        "без донатов, без читов, без команд.\n\n"
+        "Чистая ванилка 🍀"
+    )
+
+    with open("info_foto.png", "rb") as photo:
+        bot.send_photo(
+            msg.chat.id,
+            photo,
+            caption=text,
+            reply_markup=keyboard
+        )
+
+bot.polling(none_stop=True)
